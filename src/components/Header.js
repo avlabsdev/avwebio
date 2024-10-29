7d2ce6ec8cc65cb89ee5215c98067cf764128fa8
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-29 22:23:14 +0000

    added asset, comp and page updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header className="p-4 py-4 bg-neutral-950 border-b border-neutral-900 mb-4">
+        <header className="bg-neutral-950 border-b border-neutral-900 mb-4">
             <div className="container mx-auto flex justify-between items-center">
-                <a href="#/" className="font-medium">AV Web IO</a>
-                <nav className="flex gap-8 items-center">
-                    <a href="#/">Purpose</a>
-                    <a href="#/">Projects</a>
-                    <a href="#/">Resources</a>
-                    <a href="#/">Inquire</a>
+                <a href="#/" className="font-medium" p-4>AV Web IO</a>
+                <nav className="flex gap-8 items-center text-neutral-400 font-medium">
+                    <a href="#/" className="hover:text-neutral-300 p-4">Purpose</a>
+                    <a href="#/" className="hover:text-neutral-300 p-4">Projects</a>
+                    <a href="#/" className="hover:text-neutral-300 p-4">Resources</a>
+                    <a href="#/" className="hover:text-neutral-300 p-4">Inquire</a>
                 </nav>
             </div>
         </header>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import futureImage from '../images/hero-image.png';
+
 const HomePage = () => {
   return (
-    <div className="container mx-auto flex flex-col gap-8">
-      <h1>Under construction!</h1>
+    <div className="max-h-screen overflow-hidden">
+      <div className="container mx-auto flex justify-center items-center mt-24">
+        <h1 className="flex flex-col gap-4 text-5xl text-center"><span>We're going places <i>fast</i>.</span><span className="text-8xl font-bold text-neutral-300">There's no time to wait.</span></h1>
+      </div>
+      <img src={futureImage} alt="Futuristic space suit" className="-mt-80 w-full" />
     </div>
   );
 }
