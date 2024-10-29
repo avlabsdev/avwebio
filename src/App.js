c29a597a6449dc47d3bb701a94acdac4003da189
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-29 21:53:32 +0000

    react setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Under construction!</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container mx-auto py-4 flex flex-col gap-8 px-4">
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default HomePage;
